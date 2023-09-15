0bc5b4d9f2123c44d1f83135a4721870a37f9c7c
SujalDhiman <sujaldhiman2003@gmail.com>
2023-09-15 23:42:12 +0530

    Added Dashboard+ Done With set Status and stored context in local

--- frontend/busApp/src/busDashboard/BusDashboard.jsx
+++ frontend/busApp/src/busDashboard/BusDashboard.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 function BusDashboard()
 {
-
-    const {socket,busId,setBusId}=useContext(SocketContext)
+    const busId=localStorage.getItem("id")
+    // const {socket,busId,setBusId}=useContext(SocketContext)
     console.log(busId)
     return (
         <div className="mt-20 flex items-center justify-center space-x-20 text-white text-3xl">
             <Link to="/driver/dashboard">Home</Link>
             <Link to={`/driver/sendLocation/${busId}`}>Monitor Location</Link>
-            <Link to="#">Update Status</Link>
+            <Link to={`/driver/setStatus/${busId}`}>Update Status</Link>
         </div>
     )
 }
