7b3b76784243426c561c24ca3c13381d0c6ae521
Shreesh <shreeshnautiyal3@gmail.com>
2023-09-16 17:55:53 +0530

    visualized routes on map

--- frontend/busApp/src/Driver/SendLocation/SendLocation.jsx
+++ frontend/busApp/src/Driver/SendLocation/SendLocation.jsx
@@ -1,9 +1,19 @@
 import {useState, useEffect, useContext, useDeferredValue} from "react"
-import {GoogleMap,MarkerF,useLoadScript} from "@react-google-maps/api"
+import {
+    DirectionsRenderer,
+    DirectionsService,
+    GoogleMap,
+    MarkerF,
+    Polyline,
+    TrafficLayer,
+    useLoadScript
+} from "@react-google-maps/api"
 import "./SendLocation.css"
 import { SocketContext } from "../../Context/SocketContext"
 import { useParams } from "react-router-dom"
 import {MAPS_KEY} from "../../Constants/keys.js";
+import axios from "axios";
+import {axiosConfig, SERVER_URL} from "../../Constants/config.js";
 
 
 
@@ -17,6 +27,10 @@ function Map()
     //To prevent react batch rendering problems
     const differedLatitude = useDeferredValue(latitude);
     const differedLongitude = useDeferredValue(longitude);
+
+    const [directionsResponse, setDirectionsResponse] = useState(null);
+    const directionsService = new google.maps.DirectionsService();
+
 
     const {id}=useParams()
 
@@ -32,7 +46,6 @@ function Map()
         }
 
         navigator.geolocation.watchPosition((position)=>{
-            console.log(position.coords);
             setLatitude(position.coords.latitude);
             setLongitude(position.coords.longitude);
         }, error, options);
@@ -41,15 +54,48 @@ function Map()
     },[latitude,longitude])
 
     useEffect(() => {
+        (async () => {
+            const route = await axios.get(`${SERVER_URL}/api/v1/activeBus/${id}`, axiosConfig);
+            const waypoints = [];
+            route.data.bus.route.stations.map((station) => {
+              waypoints.push({"location": `${station.position[0]} ${station.position[1]}`, "stopover": true});
+            });
+
+            console.log(waypoints);
+
+            const directionsOptions = {
+                destination: waypoints[waypoints.length-1].location ,
+                origin: waypoints[0].location,
+                waypoints: waypoints,
+                travelMode: 'DRIVING',
+            };
+
+            directionsService.route(directionsOptions, (response, status) => {
+                if (status === 'OK') {
+                    setDirectionsResponse(response);
+                } else {
+                    console.log('Directions request failed: ', status);
+                }
+            });
+        })();
+    }, [])
+
+    useEffect(() => {
         //Only emitting when there is a change in position
         socket.emit(`busId`,{latitude: differedLatitude, longitude: differedLongitude,id})
         console.log(differedLatitude, differedLongitude);
     }, [differedLatitude, differedLongitude])
 
+
+
     return (
         <>
         <div><h1 className="text-white">latitude :{differedLatitude}  longitude : {differedLongitude} </h1></div>
             <GoogleMap zoom={10} center={{lat:differedLatitude,lng:differedLongitude}} mapContainerClassName="map-container">
+                {directionsResponse && <>
+                    <TrafficLayer/>
+                    <DirectionsRenderer options={{ directions: directionsResponse }}/>
+                </>}
                 <MarkerF position={{lat:differedLatitude,lng:differedLongitude}} />
             </GoogleMap>
         </>
