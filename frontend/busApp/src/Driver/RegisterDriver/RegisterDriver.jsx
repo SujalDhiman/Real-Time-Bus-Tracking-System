b369a8a98e49bc9f52f88589f0cd9e12076e3f21
codeudi <uditaggarwal111@gmail.com>
2023-09-19 20:02:21 +0530

    Updated repositary to main

--- frontend/busApp/src/Driver/RegisterDriver/RegisterDriver.jsx
+++ frontend/busApp/src/Driver/RegisterDriver/RegisterDriver.jsx
@@ -77,53 +77,59 @@ function RegisterDriver()
     }
 
     return (
-    <div className="w-screen h-screen flex overflow-hidden"> 
-    <div className="flex flex-col justify-center w-1/3 bg-[#0F232C] h-[100vh]">
-        <div className="flex justify-evenly items-center pt-10 mt-16">
-            <button className={`px-4 py-2 text-2xl font-semibold ${page === "Sign In" ?"text-white" : "text-orange-600"  }`}  onClick={changePage}>Register</button>
-            <button className={`px-4 py-2 text-2xl font-semibold ${page === "Register" ?"text-white":"text-orange-600"}`} onClick={changePage}>Sign In</button>
+    <div className="flex h-[100vh] w-[100vw] overflow-hidden font-comic-neue"> 
+    <div className="bg-[#F5FDFF] h-[100vh] w-[35%] flex flex-col items-center">
+        <div className="flex flex-col mt-[6vh]">
+            <div className="flex space-x-20">
+            <button className={` text-2xl ${page === "Sign In" ?"text-[#BAA2A2]" : "text-black" }`}  onClick={changePage}>Register</button>
+            <button className={`text-2xl ${page === "Register" ?"text-[#BAA2A2]":"text-black"}`} onClick={changePage}>Sign In</button>
+            </div>
+            <div>
+            <div className={`shadow-xl rounded-lg line bg-[#E01A27] h-[5px] w-[35%] transition-transform ${page === "Register" ? "translate-x-0" : "translate-x-[160px]"}`}></div>
+            </div>
         </div>
         {page === "Register"?(
-            <div className="mt-5 flex flex-col justify-center items-center">
-            <div className="rounded-lg w-2/3 h-screen  flex flex-col  items-center space-y-5">
-                <div className="box-border flex bg-black items-center px-4 rounded-md"> 
-                <input type="text" className="outline-none bg-black text-white h-[50px] " value={busNumber} onChange={(e)=>setBusNumber(e.target.value)} placeholder="Enter Bus Number"/>
+            <div className="mt-[4vh]">
+            <div className="flex flex-col items-center space-y-[4vh]">
+                <div className="flex bg-white border border-black w-[290px] h-[50px] justify-between items-center rounded-lg p-2 shadow-xl">
+                <input type="text" className="placeholder-black focus:outline-none" value={busNumber} onChange={(e) => setBusNumber(e.target.value)} placeholder="Bus Number"/>
                 <BusNumberSVG />
                 </div>
 
-                <div className="flex  bg-black items-center px-4 rounded-md">
-                <input type="text" value={busNumberPlate}  className="outline-none  bg-black  text-white h-[50px]" onChange={(e)=>setBusNumberPlate(e.target.value)} placeholder="Enter Bus Number Plate"/>
+                <div className="flex bg-white border border-black w-[290px] h-[50px] justify-between items-center rounded-lg p-2 shadow-xl">
+                <input type="text" value={busNumberPlate}  className="placeholder-black focus:outline-none" onChange={(e)=>setBusNumberPlate(e.target.value)} placeholder="Registration number"/>
                 <BusNumberPlate />
                 </div>
 
-                <div className="flex  bg-black items-center px-4 rounded-md">
-                <input type="text" value={name}  className="outline-none  bg-black  text-white h-[50px]" onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"/>
+                <div className="flex bg-white border border-black w-[290px] h-[50px] justify-between items-center rounded-lg p-2 shadow-xl">
+                <input type="text" value={name}  className="placeholder-black focus:outline-none" onChange={(e)=>setName(e.target.value)} placeholder="Name"/>
                 <UserSVG />
                 </div>
 
-                <div className="flex  bg-black items-center px-4 rounded-md">
-                <input type="text" value={contact}  className="outline-none  bg-black text-white h-[50px]"  onChange={(e)=>setContact(e.target.value)} placeholder="Enter Contact Number"/>
+                <div className="flex bg-white border border-black w-[290px] h-[50px] justify-between items-center rounded-lg p-2 shadow-xl">
+                <input type="text" value={contact}  className="placeholder-black focus:outline-none"  onChange={(e)=>setContact(e.target.value)} placeholder="Contact Number"/>
                 <ContactSVG />
                 </div>    
 
-                <div className="flex  bg-black  items-center rounded-md">
-                    <input type={toggle} value={password}  className="outline-none  bg-black text-white h-[50px] px-2 rounded-md" onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Your Password"/>
-                    <button onClick={viewPassword} className="px-4 py-1 text-white">{toggle === "password"? <PasswordSVG />:<HidePasswordSVG />}</button>
+                <div className="flex bg-white border border-black w-[290px] h-[50px] justify-between items-center rounded-lg p-2 shadow-xl">
+                    <input type={toggle} value={password}  className="placeholder-black focus:outline-none" onChange={(e)=>setPassword(e.target.value)} placeholder="Password"/>
+                    <button onClick={viewPassword} className="">{toggle === "password"? <PasswordSVG />:<HidePasswordSVG />}</button>
                 </div>
                 
-                <div className="flex pl-20">
-                    <input type="file" accept="image/*" onChange={(e)=>
+                <div className="flex items-center h-[50px] w-[290px]">
+                    <span className="text-[23px] mr-[6px]">PUC:</span>
+                    <input className="text-[#5F5E5E] border-none" type="file" accept="image/*" onChange={(e)=>
                         {
                             setFileDetails(e.target.files[0])}
                         }/>
                 </div>
 
-                <div className="flex flex-col items-center justify-center space-y-5">
-                    <h1 className="text-white">Have an account already ? <button onClick={changePage} className="text-yellow-500 font-semibold ">Sign In</button></h1>
+                <div className="flex flex-col items-center space-y-5">
+                    <h1 className="text-[15px]">Have an account already ? <button onClick={changePage} className="text-[#E93F4B]">Sign In</button></h1>
                     {isLoading ? (
                        <div className="animate-spin rounded-full h-6 w-6 border-t-4 border-b-4 border-l-3 border-r-3 border-yellow-500 mx-auto"></div>
                     ) : (
-                         <button className="px-12 py-2 rounded-xl font-bold  text-white bg-[#004D52] hover:text-gray-300" onClick={submitDetails}>Submit</button> 
+                         <button className="px-12 py-3 w-[200px] rounded-full font-bold  text-white bg-[#E93F4B] hover:text-gray-300 text-[20px]" onClick={submitDetails}>Submit</button> 
                     )}
                    
                     
@@ -133,8 +139,8 @@ function RegisterDriver()
             <LoginDriver/>
         ) }
     </div>
-    <div className="flex object-cover">
-        <img src={main} alt="photo"/>
+    <div>
+        <img src={main} alt="photo" className="h-[100vh] w-[100vw]"/>
     </div>
     <ToastContainer transition={Zoom}/>
     </div>
